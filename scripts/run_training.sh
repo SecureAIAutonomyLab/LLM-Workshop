7065c1779ae6e3b7caeb2cfef31d64370813db6c
brandonwherry <bwh1126@gmail.com>
2023-07-05 21:03:24 -0500

    adjusted values

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+export TRANSFORMERS_VERBOSITY=info
+export DS_SKIP_CUDA_CHECK=0
 
 torchrun --nproc_per_node=8 --master_port=2345 ../src/llm-workshop/train.py \
     --model_name_or_path ../llama-7b-hf \
     --data_path vicgalle/alpaca-gpt4 \
-    --output_dir ../training_output1 \
-    --num_train_epochs 3 \
+    --output_dir ../training_output \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 8 \
@@ -15,6 +17,6 @@ torchrun --nproc_per_node=8 --master_port=2345 ../src/llm-workshop/train.py \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
-    --bf16 \
+    --fp16 \
     --gradient_checkpointing \
-    --deepspeed "../configs/ds_config_dgx.json"
+    --deepspeed "../configs/ds_config_arc.json"
